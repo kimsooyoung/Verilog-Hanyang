1b93d4ce905a444ce2472082027d2ad21a62c605
kimsooyoung <tge1375@naver.com>
2021-06-23 15:28:55 +0900

    Done : Advanced_pipeline

--- week6_basic_pipeline/advanced_pipeline_debug.v
+++ week6_basic_pipeline/advanced_pipeline_debug.v
@@ -21,8 +21,9 @@
 
 module advanced_pipeline (clk, reset, result, PC_now, Instruction_now,
 	Rs_now, Rt_now, ALU_input_1, ALU_input_2, immi_Shifted, PC_4,
-	load_data, Beq_address, Write_Register,
+	load_data, Beq_address, J_address, Write_Register,
 	debug_flag_2, Debug_RegWrite, Debug_Write_Data,
+	Debug_PCWrite, Debug_IF_ID_Write, Debug_IF_Flush,
 	Debug_ForWardA, Debug_ForWardB);
 
 	input clk, reset;
@@ -32,9 +33,11 @@ module advanced_pipeline (clk, reset, result, PC_now, Instruction_now,
 	output [31:0] Rs_now, Rt_now; // output from Register File 
 	output [31:0] ALU_input_1, ALU_input_2; // ALU inputs
 	output [31:0] PC_4, Beq_address, immi_Shifted; // for Branch Address Debugging
+	output [31:0] J_address; // Jump Address
 	output [4:0] Write_Register; // Rd value
 	output [31:0] debug_flag_2, Debug_Write_Data; // ALU_result in MEM stage, Write_Data for Register File
 	output Debug_RegWrite; // RegWrite Op Flag
+	output Debug_PCWrite, Debug_IF_ID_Write, Debug_IF_Flush;
 
 	output [1:0] Debug_ForWardA, Debug_ForWardB;
 
@@ -44,16 +47,29 @@ module advanced_pipeline (clk, reset, result, PC_now, Instruction_now,
 	wire [31:0] PC_out;
 	wire [31:0] PC_plus4;
 	wire [31:0] IF_instruction;
+	wire [31:0] Branch_MUX_output;
+	wire [31:0] Jump_MUX_output;
+	wire PCWrite;
+	wire IF_ID_Write;
+	wire Branch_taken;
 
 	// ID stage
 	wire [31:0] ID_PC_plus4;
 	wire [31:0] ID_instruction;
 	wire [31:0] Read_data_1, Read_data_2;
 	wire [31:0] sign_extended_immi;
+	wire [31:0] Jump_Address;
+	wire [31:0] ID_Shifted_immi;
+	wire [31:0] ID_Branch_addr;
 	wire ALUSrc, RegWrite;
 	wire RegDst, Jump, Branch;
 	wire MemRead, MemtoReg, MemWrite;
+	wire RegDst_t, Jump_t, Branch_t;
+	wire MemRead_t, MemtoReg_t, MemWrite_t;
+	wire IF_Flush, ID_Flush, EX_Flush;
+	wire Rs_Rt_equal;
 	wire [1:0] ALUOp;
+	wire Hazard, Control_Reset;
 
 	// EX stage
 	wire [31:0] EX_PC_plus4;
@@ -96,19 +112,29 @@ module advanced_pipeline (clk, reset, result, PC_now, Instruction_now,
 	//////////////////////////////////
 	//// Instruction Fetch stage /////
 	//////////////////////////////////
-	N_bit_MUX #(32) pc_mux (.input0(PC_plus4), .input1(MEM_Branch_addr), 
-	 .mux_out(PC_in), .control(PCSrc));
 	Program_Counter program_counter (.clk(clk), .reset(reset),
-	 .PC_in(PC_in), .PC_out(PC_out));
+	 .PC_in(PC_in), .PC_out(PC_out), .PCWrite(PCWrite));
 	Instruction_Memory instruction_memory (.address(PC_out),
 	 .instruction(IF_instruction), .reset(reset));
 	ALU_add_only pc_add_4 (.input1(PC_out), .input2(32'b0100), .add_out(PC_plus4));
+	
+	assign Debug_PCWrite = PCWrite;
+	assign Debug_IF_Flush = IF_Flush;
+
+	and (Branch_taken, Branch, Rs_Rt_equal);
+	N_bit_MUX #(32) branch_mux (.input0(PC_plus4), .input1(ID_Branch_addr), 
+	 .mux_out(Branch_MUX_output), .control(Branch_taken));
+	N_bit_MUX #(32) jump_mux (.input0(Branch_MUX_output), .input1(Jump_Address), 
+	 .mux_out(PC_in), .control(Jump));
+	
 	IF_ID_Stage_Reg IF_ID_Stage_Unit (.clk(clk), .reset(reset),
+	 .IF_ID_Write(IF_ID_Write), .IF_Flush(IF_Flush),
 	 .PC_plus4_in(PC_plus4), .PC_plus4_out(ID_PC_plus4),
 	 .instruction_in(IF_instruction), .instruction_out(ID_instruction));
 	
 	assign PC_now = PC_out;
 	assign Instruction_now = IF_instruction;
+	assign Debug_IF_ID_Write = IF_ID_Write;
 
 	//////////////////////////////////
 	//// Instruction Decode stage ////
@@ -120,21 +146,46 @@ module advanced_pipeline (clk, reset, result, PC_now, Instruction_now,
 	 .Read_Register_1(ID_instruction[25:21]), 
 	 .Read_Register_2(ID_instruction[20:16]), 
 	 .Write_Register(WB_Write_Register), .Write_Data(WB_Write_Data),
-	 .Read_Data_1(Read_data_1), .Read_Data_2(Read_data_2), .RegWrite(WB_RegWrite));
+	 .Read_Data_1(Read_data_1), .Read_Data_2(Read_data_2), 
+	 .RegWrite(WB_RegWrite));
+	assign Rs_Rt_equal = (Read_data_1 == Read_data_2)?1:0;
+	assign Jump_Address = {ID_PC_plus4[31:28], ID_instruction[25:0], 2'b00}; 
 	Sign_Extension immi_sign_extension (.input_16(ID_instruction[15:0]), .output_32(sign_extended_immi));
+	assign ID_Shifted_immi = { sign_extended_immi[29:0], 2'b00 };
+	ALU_add_only alu_add_only_unit (.input1(ID_PC_plus4), 
+	.input2(ID_Shifted_immi), .add_out(ID_Branch_addr)); 
+
+	assign immi_Shifted = ID_Shifted_immi;
+	assign Beq_address = ID_Branch_addr;
+	assign J_address = Jump_Address;
+
+ 	Hazard_Detection_Unit hazard_detection_unit (.ID_EX_RegisterRt(EX_instruction[20:16]), .ID_EX_MemRead(EX_MemRead), 
+	 .IF_ID_RegisterRs(ID_instruction[25:21]), .IF_ID_RegisterRt(ID_instruction[20:16]),
+	 .PCWrite(PCWrite), .IF_ID_Write(IF_ID_Write), .Hazard(Hazard));
 	Control control_unit (.OpCode(ID_instruction[31:26]),
 	 .RegDst(RegDst), .Jump(Jump), .Branch(Branch), 
 	 .MemRead(MemRead), .MemtoReg(MemtoReg), .ALUOp(ALUOp), 
-	 .MemWrite(MemWrite), .ALUSrc(ALUSrc), .RegWrite(RegWrite));
+	 .MemWrite(MemWrite), .ALUSrc(ALUSrc), .RegWrite(RegWrite),
+	 .IF_Flush(IF_Flush), .ID_Flush(ID_Flush), .EX_Flush(EX_Flush));
+	
+	or (Control_Reset, ID_Flush, Hazard);
+	Reset_Control reset_control_unit(
+	 .RegDst_in(RegDst), .ALUSrc_in(ALUSrc), .Branch_in(Branch), 
+	 .MemRead_in(MemRead), .MemtoReg_in(MemtoReg), 
+	 .MemWrite_in(MemWrite), .RegWrite_in(RegWrite), 
+	 .RegDst_out(RegDst_t), .ALUSrc_out(ALUSrc_t), .Branch_out(Branch_t), 
+	 .MemRead_out(MemRead_t), .MemtoReg_out(MemtoReg_t), 
+	 .MemWrite_out(MemWrite_t), .RegWrite_out(RegWrite_t),
+	 .Control_Reset(Control_Reset));
 	ID_EX_Stage_Reg ID_EX_Stage_Unit (.clk(clk), .reset(reset),
-	 .RegWrite_in(RegWrite), .RegWrite_out(EX_RegWrite),
-	 .MemtoReg_in(MemtoReg), .MemtoReg_out(EX_MemtoReg),
-	 .Branch_in(Branch), .Branch_out(EX_Branch),
-	 .MemRead_in(MemRead), .MemRead_out(EX_MemRead),
-	 .MemWrite_in(MemWrite), .MemWrite_out(EX_MemWrite), 
-	 .Jump_in(Jump), .Jump_out(EX_Jump),   
-	 .RegDst_in(RegDst), .RegDst_out(EX_RegDst),
-	 .ALUSrc_in(ALUSrc), .ALUSrc_out(EX_ALUSrc), 
+	 .RegWrite_in(RegWrite_t), .RegWrite_out(EX_RegWrite),
+	 .MemtoReg_in(MemtoReg_t), .MemtoReg_out(EX_MemtoReg),
+	 .Branch_in(Branch_t), .Branch_out(EX_Branch),
+	 .MemRead_in(MemRead_t), .MemRead_out(EX_MemRead),
+	 .MemWrite_in(MemWrite_t), .MemWrite_out(EX_MemWrite), 
+	 .Jump_in(Jump), .Jump_out(EX_Jump),
+	 .RegDst_in(RegDst_t), .RegDst_out(EX_RegDst),
+	 .ALUSrc_in(ALUSrc_t), .ALUSrc_out(EX_ALUSrc), 
 	 .ALUOp_in(ALUOp), .ALUOp_out(EX_ALUOp), 
 	 .PC_plus4_in(ID_PC_plus4), .PC_plus4_out(EX_PC_plus4),
 	 .read_data_1_in(Read_data_1), .read_data_1_out(EX_Read_data_1),
@@ -168,8 +219,11 @@ module advanced_pipeline (clk, reset, result, PC_now, Instruction_now,
 	 .mux_out(ALU_input_B), .control(EX_ALUSrc));
 	ALU alu_unit (.input1(muxA_out), .input2(ALU_input_B), 
 	 .alu_out(ALU_result), .zero(ALU_zero), .control(operation_code));	
-	assign Shifted_immi = { EX_sign_extended_immi[29:0], 2'b00 };
-	ALU_add_only alu_add_only_unit (.input1(EX_PC_plus4), .input2(Shifted_immi), .add_out(Branch_addr)); 
+	
+	// assign Shifted_immi = { EX_sign_extended_immi[29:0], 2'b00 };
+	// ALU_add_only alu_add_only_unit (.input1(EX_PC_plus4), 
+	// .input2(Shifted_immi), .add_out(Branch_addr)); 
+	
 	EX_MEM_Stage_Reg EX_MEM_Stage_Unit ( .clk(clk), .reset(reset),
 	.RegWrite_in(EX_RegWrite), .RegWrite_out(MEM_RegWrite),
 	.MemtoReg_in(EX_MemtoReg), .MemtoReg_out(MEM_MemtoReg),
@@ -177,17 +231,15 @@ module advanced_pipeline (clk, reset, result, PC_now, Instruction_now,
 	.MemRead_in(EX_MemRead), .MemRead_out(MEM_MemRead),
 	.MemWrite_in(EX_MemWrite),.MemWrite_out(MEM_MemWrite),
 	.Jump_in(EX_Jump), .Jump_out(MEM_Jump),
-	.Branch_addr_in(Branch_addr), .Branch_addr_out(MEM_Branch_addr),
+	// .Branch_addr_in(Branch_addr), .Branch_addr_out(MEM_Branch_addr),
 	.ALU_zero_in(ALU_zero), .ALU_zero_out(MEM_ALU_zero),
 	.ALU_result_in(ALU_result), .ALU_result_out(MEM_ALU_result),
 	.Read_data_2_in(EX_Read_data_2), .Read_data_2_out(MEM_Read_data_2), 
 	.RegisterRd_in(EX_Write_Register), .RegisterRd_out(MEM_Write_Register));
 
 	assign PC_4 = EX_PC_plus4;
-	assign immi_Shifted = Shifted_immi;
 	assign ALU_input_1 = muxA_out;
 	assign ALU_input_2 = ALU_input_B;
-	assign Beq_address = Branch_addr;
 	
 	assign result = ALU_result;
 
@@ -216,16 +268,24 @@ module advanced_pipeline (clk, reset, result, PC_now, Instruction_now,
 	 .mux_out(WB_Write_Data), .control(WB_MemtoReg));
 
 	assign Write_Register = WB_Write_Register;
+
+
+
+
 endmodule
 
 // IF/ID stage register
-module IF_ID_Stage_Reg (clk, reset, PC_plus4_in, PC_plus4_out, 
-	instruction_in, instruction_out);
+module IF_ID_Stage_Reg (clk, reset, 
+	PC_plus4_in, PC_plus4_out, 
+	instruction_in, instruction_out,
+	IF_ID_Write, IF_Flush);
 
 	input clk, reset;
 	input [31:0] PC_plus4_in, instruction_in;
-
 	output reg [31:0] PC_plus4_out, instruction_out;
+
+	// Hazard
+	input IF_Flush, IF_ID_Write;
 
 	always @(posedge clk or negedge reset) begin
 
@@ -234,7 +294,14 @@ module IF_ID_Stage_Reg (clk, reset, PC_plus4_in, PC_plus4_out,
 			instruction_out <= 32'b0;
 		end
 
-		else begin
+		else if (IF_Flush) begin
+			// TODO : Check condition
+			// PC_plus4_out <= 32'b0;
+			// PC_plus4_out <= PC_plus4_in;
+			instruction_out <= 32'b0;
+		end	
+
+		else if (IF_ID_Write) begin
 			PC_plus4_out <= PC_plus4_in;
 			instruction_out <= instruction_in;
 		end
@@ -308,7 +375,8 @@ module EX_MEM_Stage_Reg (clk, reset,
 	RegWrite_in, RegWrite_out, MemtoReg_in, MemtoReg_out, 
 	Branch_in, Branch_out, MemRead_in, MemRead_out, 
 	MemWrite_in, MemWrite_out, Jump_in, Jump_out,
-	Branch_addr_in, Branch_addr_out, ALU_zero_in, ALU_zero_out, 
+	// Branch_addr_in, Branch_addr_out, 
+	ALU_zero_in, ALU_zero_out, 
 	ALU_result_in, ALU_result_out, Read_data_2_in, Read_data_2_out, 
 	RegisterRd_in, RegisterRd_out);
 
@@ -319,8 +387,8 @@ module EX_MEM_Stage_Reg (clk, reset,
 	input Branch_in, MemRead_in, MemWrite_in, Jump_in;
 	output reg Branch_out, MemRead_out, MemWrite_out, Jump_out;
 	// addr content
-	input [31:0] Branch_addr_in;
-	output reg [31:0] Branch_addr_out;
+	// input [31:0] Branch_addr_in;
+	// output reg [31:0] Branch_addr_out;
 	// data content
 	input ALU_zero_in;
 	output reg ALU_zero_out;
@@ -338,7 +406,8 @@ module EX_MEM_Stage_Reg (clk, reset,
 		  RegWrite_out <= 1'b0; MemtoReg_out <= 1'b0;
 		  Branch_out <= 1'b0; MemRead_out <= 1'b0;
 		  MemWrite_out <= 1'b0; Jump_out <= 1'b0;
-		  Branch_addr_out <= 32'b0; ALU_zero_out <= 1'b0;
+		//   Branch_addr_out <= 32'b0; 
+		  ALU_zero_out <= 1'b0;
 		  ALU_result_out <= 32'b0; Read_data_2_out <= 32'b0;
 		  RegisterRd_out <= 5'b0; 
 		end
@@ -347,7 +416,8 @@ module EX_MEM_Stage_Reg (clk, reset,
 		  RegWrite_out <= RegWrite_in; MemtoReg_out <= MemtoReg_in;
 		  Branch_out <= Branch_in; MemRead_out <= MemRead_in;
 		  MemWrite_out <= MemWrite_in; Jump_out <= Jump_in;
-		  Branch_addr_out <= Branch_addr_in; ALU_zero_out <= ALU_zero_in;
+		//   Branch_addr_out <= Branch_addr_in; 
+		  ALU_zero_out <= ALU_zero_in;
 		  ALU_result_out <= ALU_result_in; Read_data_2_out <= Read_data_2_in;
 		  RegisterRd_out <= RegisterRd_in;
 		end
@@ -390,8 +460,9 @@ module MEM_WB_Stage_Reg (RegWrite_in, RegWrite_out,
 endmodule
 
 // PC
-module Program_Counter (clk, reset, PC_in, PC_out);
+module Program_Counter (clk, reset, PC_in, PC_out, PCWrite);
 
+	input PCWrite;
 	input clk, reset;
 	input [31:0] PC_in;
 
@@ -400,7 +471,7 @@ module Program_Counter (clk, reset, PC_in, PC_out);
 	always @ (posedge clk or negedge reset)	begin
 		if (!reset)
 			PC_out <= 0;
-		else
+		else if (PCWrite)
 			PC_out <= PC_in;
 	end
 endmodule
@@ -427,7 +498,7 @@ module Instruction_Memory (address, instruction, reset);
 		mem[1] = 32'b000000_00001_00101_00100_00000_100010; // sub $4, $1, $5
 		mem[2] = 32'b000000_00001_00111_00110_00000_100100; // and $6, $1, $7
 		mem[3] = 32'b000000_00001_01001_01000_00000_100101; // or $8, $1, $9
-		mem[5] = 32'b000100_00110_00000_1111111111111011; // beq $6, $0, Label (-5)
+		mem[4] = 32'b000100_00110_00000_1111111111111011; // beq $6, $0, Label (-5)
 		
 		// mem[0] = 32'b000000_00011_00100_00010_00000_100000; // add $2, $3, $4
 		// mem[1] = 32'b000000_00011_00100_00001_00000_100010; // sub $1, $3, $4
@@ -467,11 +538,10 @@ module Register_File (Read_Register_1, Read_Register_2,
 	input clk, reset, RegWrite;
 	output reg [31:0] Read_Data_1, Read_Data_2;
 
-
 	reg [31:0] mem [31:0];
 	integer k;
  	
-	always @(posedge clk or negedge reset) begin
+	always @(*) begin
 		if (!reset) begin
 			for (k = 0; k < 32; k = k + 1) begin
 				mem[k] <= 32'b0;
@@ -509,11 +579,14 @@ module Sign_Extension (input_16, output_32);
 endmodule
 
 // Control Path
-module Control (OpCode, RegDst, Jump, Branch, MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite);
+module Control (OpCode, RegDst,  MemRead, MemtoReg, ALUOp, MemWrite, ALUSrc, RegWrite,
+	Jump, Branch, IF_Flush, ID_Flush, EX_Flush);
+
 	input [5:0] OpCode;
 	output [1:0] ALUOp;
-	output RegDst, Jump, Branch, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
-
+	output RegDst, MemRead, MemtoReg, MemWrite, ALUSrc, RegWrite;
+	output Jump, Branch;
+	output IF_Flush, ID_Flush, EX_Flush;
 	// 000000 : add, sub, and, or, slt
 	// 001000 : addi 
 	// 100011 : lw
@@ -530,8 +603,6 @@ module Control (OpCode, RegDst, Jump, Branch, MemRead, MemtoReg, ALUOp, MemWrite
 	// 100011 (lw), 101011 (sw)
 	assign ALUSrc=((OpCode[5])&(~OpCode[4])&(~OpCode[3])&(~OpCode[2])&(OpCode[1])&(OpCode[0]))  | 
 					  ((OpCode[5])&(~OpCode[4])&(OpCode[3])&(~OpCode[2])&(OpCode[1])&(OpCode[0])); 
-	// 000100 (beq)
-	assign Branch=(~OpCode[5])&(~OpCode[4])&(~OpCode[3])&(OpCode[2])&(~OpCode[1])&(~OpCode[0]);
 	// 100011 (lw)
 	assign MemRead=(OpCode[5])&(~OpCode[4])&(~OpCode[3])&(~OpCode[2])&(OpCode[1])&(OpCode[0]);
 	// 101011 (sw)
@@ -542,8 +613,20 @@ module Control (OpCode, RegDst, Jump, Branch, MemRead, MemtoReg, ALUOp, MemWrite
 	assign RegWrite=((~OpCode[5])&(~OpCode[4])&(~OpCode[3])&(~OpCode[2])&(~OpCode[1])&(~OpCode[0]))|
  	                ((~OpCode[5])&(~OpCode[4])&(OpCode[3])&(~OpCode[2])&(~OpCode[1])&(~OpCode[0])) |
 						 ((OpCode[5])&(~OpCode[4])&(~OpCode[3])&(~OpCode[2])&(OpCode[1])&(OpCode[0]));
+
+	// 000100 (beq)
+	assign Branch=(~OpCode[5])&(~OpCode[4])&(~OpCode[3])&(OpCode[2])&(~OpCode[1])&(~OpCode[0]);
 	// 000010 (j)
 	assign Jump=(~OpCode[5])&(~OpCode[4])&(~OpCode[3])&(~OpCode[2])&(OpCode[1])&(~OpCode[0]);
+
+	// only jump
+	assign IF_Flush=(~OpCode[5])&(~OpCode[4])&(~OpCode[3])&(~OpCode[2])&(OpCode[1])&(~OpCode[0]);
+	// jump or branch
+	assign ID_Flush=(~OpCode[5])&(~OpCode[4])&(~OpCode[3])&(~OpCode[2])&(OpCode[1])&(~OpCode[0]) |
+					(~OpCode[5])&(~OpCode[4])&(~OpCode[3])&(OpCode[2])&(~OpCode[1])&(~OpCode[0]);
+	// Umm...
+	assign EX_Flush=0;
+
 endmodule
 
 module ALU_Control (ALUOp, f_code, operation_code);
@@ -682,8 +765,8 @@ module Forwarding_Unit (EX_RegisterRs, EX_RegisterRt,
 	// assign equal_WB_ID_rs=(WB_RegisterRd==IF_ID_RegisterRs)?1:0;
 	// assign equal_WB_ID_rt=(WB_RegisterRd==IF_ID_RegisterRt)?1:0;
 
-	// wire EX_HazardA, EX_HazardB;
-	// wire WB_HazardA, WB_HazardB;
+	wire EX_HazardA, EX_HazardB;
+	wire WB_HazardA, WB_HazardB;
 	
 	assign EX_HazardA = EX_MEM_RegWrite & (MEM_RegisterRd!=0) & (MEM_RegisterRd==EX_RegisterRs);
 	assign EX_HazardB = EX_MEM_RegWrite & (MEM_RegisterRd!=0) & (MEM_RegisterRd==EX_RegisterRt);
@@ -737,25 +820,63 @@ module Forwarding_Unit (EX_RegisterRs, EX_RegisterRt,
 	// 	else
 	// 		ForwardD<=0;
 	// end
+endmodule
+
+module Hazard_Detection_Unit (ID_EX_RegisterRt, ID_EX_MemRead,
+	IF_ID_RegisterRs, IF_ID_RegisterRt, 
+	PCWrite, IF_ID_Write, Hazard);
+
+	input [4:0] IF_ID_RegisterRs, IF_ID_RegisterRt;
+	input [4:0] ID_EX_RegisterRt;
+	input ID_EX_MemRead;
+	
+	output reg PCWrite, IF_ID_Write, Hazard;
+	
+	// wire equal_IDEXrt_IFIDrs,equal_IDEXrt_IFIDrt;
+	// assign equal_IDEXrt_IFIDrs=(ID_EX_RegisterRt==IF_ID_RegisterRs)?1:0;
+	// assign equal_IDEXrt_IFIDrt=(ID_EX_RegisterRt==IF_ID_RegisterRt)?1:0;
+	
+	wire Rs_Stall, Rt_Stall;
+	assign Rs_Stall=(ID_EX_RegisterRt==IF_ID_RegisterRs);
+	assign Rt_Stall=(ID_EX_RegisterRt==IF_ID_RegisterRt);
+
+	always@(*) begin
+
+		if(ID_EX_MemRead & (Rs_Stall|Rt_Stall)) begin 
+			PCWrite <= 0;
+			IF_ID_Write <=0; 
+			Hazard <= 1; 
+		end
+		
+		else begin 
+			PCWrite <= 1;
+			IF_ID_Write <= 1;
+			Hazard <= 0; 
+		end
+	end
 
 endmodule
 
-module stall_for_lw_Control (ID_EX_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt, ID_EX_MemRead, PCWrite, IF_ID_Write, ID_Flush_lwstall);
-	input [4:0] ID_EX_RegisterRt, IF_ID_RegisterRs, IF_ID_RegisterRt;
-	input ID_EX_MemRead;
-	output PCWrite, IF_ID_Write, ID_Flush_lwstall;
-	wire equal_IDEXrt_IFIDrs,equal_IDEXrt_IFIDrt;
-	assign equal_IDEXrt_IFIDrs=(ID_EX_RegisterRt==IF_ID_RegisterRs)?1:0;
-	assign equal_IDEXrt_IFIDrt=(ID_EX_RegisterRt==IF_ID_RegisterRt)?1:0;
-	reg PCWrite, IF_ID_Write, ID_Flush_lwstall;
+module Reset_Control (RegDst_in, ALUSrc_in, Branch_in, MemRead_in, 
+	MemtoReg_in, MemWrite_in, RegWrite_in, Control_Reset,
+	RegDst_out, ALUSrc_out, Branch_out, MemRead_out,
+	MemtoReg_out, MemWrite_out, RegWrite_out);
 	
-	always@(ID_EX_MemRead or equal_IDEXrt_IFIDrs or equal_IDEXrt_IFIDrt)
-	begin
-		if(ID_EX_MemRead & (equal_IDEXrt_IFIDrs|equal_IDEXrt_IFIDrt))
-		begin PCWrite<=0;IF_ID_Write<=0;ID_Flush_lwstall<=1; end
-		else 
-		begin PCWrite<=1;IF_ID_Write<=1;ID_Flush_lwstall<=0; end
-	end
+	input RegDst_in, ALUSrc_in, Branch_in, MemRead_in;
+	input MemtoReg_in, MemWrite_in, RegWrite_in;
+	input Control_Reset;
+
+	output RegDst_out, ALUSrc_out, Branch_out, MemRead_out;
+	output MemtoReg_out, MemWrite_out, RegWrite_out;
+
+	assign RegDst_out = Control_Reset ? 0 : RegDst_in;
+	assign ALUSrc_out = Control_Reset ? 0 : ALUSrc_in;
+	assign Branch_out = Control_Reset ? 0 : Branch_in;
+	assign MemRead_out = Control_Reset ? 0 : MemRead_in;
+	assign MemtoReg_out = Control_Reset ? 0 : MemtoReg_in;
+	assign MemWrite_out = Control_Reset ? 0 : MemWrite_in;
+	assign RegWrite_out = Control_Reset ? 0 : RegWrite_in;
+
 endmodule
 
 module Mux_32bit_3to1 (in00, in01, in10, mux_out, control);
